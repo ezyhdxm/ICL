fixed causal graph

base_models.py
@@ -13,98 +13,16 @@ from torch import nn, Tensor
 from torch.nn import functional as F
 from typing import List, Optional, Tuple
 from pos_encoder import *
+from attention import *
 
-
-class MultiHeadAttention(nn.Module):
-    def __init__(self, config, layer=0):
-        super().__init__()
-        self.emb_dim = config.emb_dim
-        self.n_head = config.num_heads[layer]
-        self.head_dim = self.emb_dim // self.n_head
-        assert self.emb_dim % self.n_head == 0, "Embedding dimension must be divisible by the number of heads."
-        if config.identity_query:
-            self.query = nn.Identity()
-        else:
-            self.query = nn.Linear(self.emb_dim, self.emb_dim, bias=config.bias)
-        self.key = nn.Linear(self.emb_dim, self.emb_dim, bias=config.bias)
-        self.value = nn.Linear(self.emb_dim, self.emb_dim, bias=config.bias)
-        if config.freeze_value:
-            self.value.weight.requires_grad_(False)
-        self.out = nn.Linear(self.emb_dim, self.emb_dim)
-        if config.freeze_out:
-            self.out.weight.requires_grad_(False)
-        self.mask = torch.tril(torch.ones(config.seq_len, config.seq_len)).unsqueeze(0).unsqueeze(1)
-        self.mask = self.mask.to(config.device) # TODO: make self.mask a register_buffer
-        self.get_attn = config.get_attn
-        self.pos_enc = config.pos_enc
-        self.seq_len = config.seq_len
-        self.scale = self.head_dim ** 0.5
-        self.flash = config.flash
-        self.counter = 0
-        self.dropout = config.dropout if config.dropout else 0.
-        assert not (self.flash and self.pos_enc == "rpe"), "Flash Attention does not support RPE currently."  
-        if self.pos_enc == "rpe":
-            self.PEK = RelativePositionalEncoding(self.head_dim, self.pos_max_len) # (T,T,D)
-            self.PEV = RelativePositionalEncoding(self.head_dim, self.pos_max_len) # (T,T,D)
-        elif self.pos_enc == "rotary":
-            self.rotary_emb = RotaryPositionalEmbeddings(self.head_dim, self.pos_max_len)
-        elif self.pos_enc == "alibi":
-            self.alibi_emb = AliBiPositionalEncoding(self.n_head)
-        
-
-    def forward(self, x): # x: (B,T,C)
-        batch_size, seq_len, _ = x.size()
-        Q = self.query(x).view(batch_size, seq_len, self.n_head, self.head_dim).transpose(1,2) # (B,H,T,D)
-        K = self.key(x).view(batch_size, seq_len, self.n_head, self.head_dim).transpose(1,2) # (B,H,T,D)
-        V = self.value(x).view(batch_size, seq_len, self.n_head, self.head_dim).transpose(1,2) # (B,H,T,D)
-        if self.pos_enc == "rotary":
-            Q = self.rotary_emb(Q)
-            K = self.rotary_emb(K)
-        if self.flash and (self.get_attn==0 or (self.counter % self.get_attn != 0)):
-            # assert self.get_attn == 0, "Flash Attention does not output attentions."
-            out = F.scaled_dot_product_attention(Q, K, V, attn_mask=None, dropout_p=self.dropout, is_causal=True)
-            out = out.transpose(1,2).contiguous().view(batch_size,seq_len,-1) # (B,T,C)
-            out = self.out(out)
-            self.counter += 1
-            return out, -1
-        
-        attn_score = Q @ K.transpose(-1,-2) / self.scale # (B,H,T,T)
-        if self.pos_enc == "rpe":
-            Q2 = self.query(x).view(batch_size, seq_len, self.n_head, self.head_dim).transpose(0,1) # (T,B,H,D)
-            Q2 = Q2.contiguous().view(seq_len, batch_size*self.n_head, self.head_dim) # (T,BH,D)
-            attn_score2 = torch.matmul(Q2, self.PEK(seq_len).transpose(1,2)) # (T,BH,D) @ (T,D,T) -> (T,BH,T)
-            attn_score2 = attn_score2.view(seq_len, batch_size, self.n_head, seq_len).transpose(0,1).contiguous() # (B,H,T,T)
-            attn_score += attn_score2 / self.scale
-        elif self.pos_enc=="alibi":
-            attn_score += self.alibi_emb(self.seq_len)
-
-        attn_score = attn_score.masked_fill(self.mask==0, -float("inf"))
-        attn = F.softmax(attn_score, dim=-1) # (B,H,T,T)
-        out = attn @ V # (B,H,T,D)
-        if self.pos_enc == "rpe":
-            attn2 = attn.transpose(0,2).contiguous().view(seq_len, -1, seq_len) # (T,BH,T)
-            out2 = torch.matmul(attn2, self.PEV(seq_len)) # (T,BH,T) @ (T,T,D) -> (T,BH,D)
-            out2 = out2.view(seq_len, batch_size, -1, self.head_dim).transpose(0,2) # (B,H,T,D)
-            out += out2
-        out = out.transpose(1,2).contiguous().view(batch_size,seq_len,-1) # (B,T,C)
-        out = self.out(out)
-        if self.get_attn > 0 and (self.counter % self.get_attn == 0):
-            self.counter += 1
-            return out, attn.detach().cpu() 
-        
-        self.counter += 1
-        return out, -1
-        
-        
 
 class TFBlock(nn.Module):
     def __init__(self, config, layer=0):
         super().__init__()
         self.MHA = MultiHeadAttention(config, layer)
-        self.ln1 = nn.LayerNorm(config.emb_dim) if config.layer_norm else None
+        self.ln1 = nn.LayerNorm(config.emb_dim) if config.layer_norm else nn.Identity()
         self.mlp = None
-        self.dropout = None
-        # self.get_attn = config.get_attn
+        self.dropout = nn.Dropout(config.dropout) if config.dropout else None
 
         if config.mlp[layer]:
             assert config.ff_dim is not None, "FeedForward dimension cannot be empty."
@@ -115,18 +33,14 @@ class TFBlock(nn.Module):
                     nn.Linear(config.ff_dim, config.emb_dim)
                 )
             else:
-                self.mlp = nn.Linear(config.emb_dim, config.ff_dim)
-            self.ln2 = nn.LayerNorm(config.emb_dim) if config.layer_norm else None
-            
-        if config.dropout:
-            self.dropout = nn.Dropout(config.dropout)
+                self.mlp = nn.Linear(config.emb_dim, config.emb_dim)
+            self.ln2 = nn.LayerNorm(config.emb_dim) if config.layer_norm else nn.Identity()
 
     def forward(self, x):
         attn_map = -1
         atten_out, attn_map = self.MHA(x)
-        x = x + self.dropout(atten_out) if self.dropout else x + atten_out
-        if self.ln1 is not None:
-            x = self.ln1(x)
+        x = x + self.dropout(atten_out) if self.dropout is not None else x + atten_out
+        x = self.ln1(x)
         if self.mlp is not None:
             mlp_out = self.mlp(x)
             if self.dropout is not None:
@@ -143,9 +57,6 @@ class Transformer(nn.Module):
         if config.pos_enc == "abs":
             self.positional_encoding = nn.Embedding(config.seq_len, config.emb_dim)
         self.layers = nn.ModuleList([TFBlock(config, layer) for layer in range(config.num_layers)])
-        #else:
-        #    self.layers = nn.ModuleList([TFBlock(config) for _ in range(config.num_layers)])
-
         self.output_layer = nn.Linear(config.emb_dim, config.vocab_size)
         self.atten_maps = {l: torch.zeros((config.num_heads[l], config.seq_len, config.seq_len)) for l in range(config.num_layers)}
 
@@ -156,7 +67,7 @@ class Transformer(nn.Module):
             x = self.embed(x)
         for i, layer in enumerate(self.layers):
             x, attn_map = layer(x)
-            if torch.is_tensor(attn_map) > 0:
+            if torch.is_tensor(attn_map):
                 self.atten_maps[i] = attn_map.mean(dim=0)
             
         logits = self.output_layer(x)

causal_graph.py
@@ -3,12 +3,17 @@ from typing import Optional, List, Tuple
 from config import CausalGraphConfig
 import numpy as np
 
-def dag_to_adj(dag):
+def dag_to_adj(dag, task_name="tree"):
     seq_len = len(dag)
     adj_mat = torch.zeros((seq_len, seq_len), dtype=torch.float)
-    py_dag = torch.tensor(dag)
-    idx = torch.where(py_dag >= 0)[0]  # Indices where dag[i] >= 0 (has a parent)
-    adj_mat[idx, py_dag[idx]] = 1  # Set adjacency matrix entries to 1
+    if task_name == "tree":
+        py_dag = torch.tensor(dag)
+        idx = torch.where(py_dag >= 0)[0]  # Indices where dag[i] >= 0 (has a parent)
+        adj_mat[idx, py_dag[idx]] = 1  # Set adjacency matrix entries to 1
+    else:
+        for i in range(seq_len):
+            if len(dag[i]) > 0:
+                adj_mat[i, dag[i]] = 1 
     return adj_mat
 
 def get_random_DAG(seq_len, p=0.5):
@@ -97,6 +102,20 @@ class InContextTreeTorch:
         y = pi[torch.arange(num_samples), test_tokens]  # Probabilities of test tokens
 
         return samples, y
+    
+    def bayes(self, samples): # samples: (B, seq_len)
+        num_samples, seq_len = samples.shape
+        test_tokens, seq = samples[:,-1], samples[:,:-1] # (B,), (B,seq_len-1)
+        counts = torch.zeros((num_samples, self.vocab_size))
+        for i in range(1, seq_len-1):
+            parent_index = self.dag[i]
+            if parent_index == -1:
+                continue
+            indices = (seq[:,parent_index] == test_tokens).nonzero(as_tuple=True)[0]
+            values = torch.ones_like(indices, dtype=torch.float) 
+            counts.index_put_((indices, seq[indices,i]), values, accumulate=True)
+        counts += self.alpha
+        return counts / counts.sum(dim=-1, keepdim=True)
 
 
 class InContextDAGTorch:
@@ -140,3 +159,18 @@ class InContextDAGTorch:
         
         return samples, p
 
+    def bayes(self, samples): # samples: (B, seq_len)
+        num_samples, seq_len = samples.shape
+        last_parents = samples[:,self.dag[-1]]
+        k = last_parents.shape[1]
+        counts = torch.zeros((num_samples, self.vocab_size))
+        for i in range(1, seq_len-1):
+            parent_indexes = self.dag[i]
+            if len(parent_indexes) == k:
+                indices = (samples[:,parent_indexes] == last_parents).all(dim=-1).nonzero(as_tuple=True)[0]
+                if len(indices) == 0:
+                    continue
+                values = torch.ones_like(indices, dtype=torch.float) 
+                counts.index_put_((indices, samples[indices,i]), values, accumulate=True)
+        counts += self.alpha
+        return counts / counts.sum(dim=-1, keepdim=True)

plot.py
@@ -5,34 +5,35 @@ from PIL import Image
 import torch
 import numpy as np
 import seaborn as sns
+import stat
+from datetime import datetime
+from causal_graph import dag_to_adj
 
 def get_loss_plots(config, train_results, task_name):
     train_losses, eval_losses, eval_steps = train_results["train_losses"], train_results["eval_losses"], train_results["eval_steps"]
     ngramLosses = train_results["ngramLosses"] if "ngramLosses" in train_results else []
+    bayes_losses = train_results["bayes_losses"] if "bayes_losses" in train_results else []
+    
     plt.figure(figsize=(8, 6))
     plt.plot(range(1, config.num_epochs + 1), train_losses, 
             linestyle='-', color='lightblue', label='Training Loss')
     plt.plot(eval_steps, eval_losses, 
              linestyle='--', color='palevioletred', label='Validation Loss')
 
-    if len(ngramLosses) >= 1:
-        plt.plot(range(1, config.num_epochs + 1), ngramLosses[0], 
-                linestyle='-', color='bisque', label='Unigram Loss')
-    if len(ngramLosses) >= 2:
-        plt.plot(range(1, config.num_epochs + 1), ngramLosses[1], 
-                linestyle='-', color='khaki', label='Bigram Loss')
-    if len(ngramLosses) >= 3:
-        plt.plot(range(1, config.num_epochs + 1), ngramLosses[2], 
-                linestyle='-', color='burlywood', label='Trigram Loss')
-    for i in range(3, len(ngramLosses)):
+    if len(bayes_losses) >= 1:
+        plt.plot(range(1, config.num_epochs + 1), bayes_losses, 
+                linestyle='-', color='burlywood', label='Bayes Loss')
+
+    for i in range(len(ngramLosses)):
         plt.plot(range(1, config.num_epochs + 1), ngramLosses[i], 
                 linestyle='-', label=f'{i+1}-gram Loss')
     
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
-    mlp = "no" if config.mlp == False else "with"
-    linear = "(linear)" if config.activation == False else "" 
-    plt.title(f'{task_name}: {config.num_heads} Heads {config.num_layers} Layers {mlp} MLP {linear} Loss Over Epochs ({config.pos_enc})')
+    is_mlp = any(config.mlp)
+    mlp = "no" if not is_mlp else "with"
+    linear = "(linear)" if (is_mlp and not any(config.activation)) else "" 
+    plt.title(f'{task_name}: {config.num_heads}-Heads {config.num_layers} Layers {mlp} MLP {linear} Loss ({config.pos_enc})')
     plt.legend()
     plt.grid()
     plt.show()
@@ -47,55 +48,89 @@ def plot_probes(train_results, config):
     
     plt.xlabel('Epochs')
     plt.ylabel('Mempry Recall & KL divergence')
-    mlp = "no" if config.mlp == False else "with"
-    linear = "(linear)" if config.activation == False else "" 
-    plt.title(f'{config.num_heads} Heads {config.num_layers} Layers {mlp} MLP {linear} Recall Over Epochs ({config.pos_enc})')
+    is_mlp = any(config.mlp)
+    mlp = "no" if not is_mlp else "with"
+    linear = "(linear)" if is_mlp and not any(config.activation) else "" 
+    plt.title(f'{config.num_heads} Heads {config.num_layers} Layers {mlp} MLP {linear} Recall ({config.pos_enc})')
     plt.legend()
     plt.grid()
     plt.show()
 
 
-def get_attn_gif(layer, head, train_results, config, task_name, folder="attns"):
+def onerror(func, path, exc_info):
+    """
+    Error handler for ``shutil.rmtree``.
+
+    If the error is due to an access error (read only file)
+    it attempts to add write permission and then retries.
+
+    If the error is for another reason it re-raises the error.
+    
+    Usage : ``shutil.rmtree(path, onerror=onerror)``
+    """
+    # Is the error an access error?
+    if not os.access(path, os.W_OK):
+        os.chmod(path, sstat.S_IWRITE)
+        func(path)
+    else:
+        raise
+
+def get_attn_gif(layer, head, train_results, config, task_name, dag=None, folder="attns", out_folder="attns_plot"):
     attn_maps = train_results["attn_maps"]
     image_paths = []
     os.makedirs(folder)
     for i, attn in attn_maps.items():
-        # Create heatmap
-        plt.figure(figsize=(6, 5))
-        plt.imshow(attn[layer][head], cmap="viridis", interpolation="nearest")
-        plt.colorbar()
-        plt.title(f"Epoch {i + 1}")
-        plt.xlabel("X-axis")
-        plt.ylabel("Y-axis")
-    
+        if dag is None:
+            plt.figure(figsize=(6, 6))
+            sns.heatmap(attn[layer][head], cmap="viridis", annot=False)
+            plt.colorbar()
+            plt.title(f"Epoch {i + 1}")
+        else:
+            adj_mat = dag_to_adj(dag, task_name)
+            if head != "both":
+                fig, axes = plt.subplots(1, 2, figsize=(12, 6))  # 1 row, 2 columns
+
+                sns.heatmap(attn[layer][head], ax=axes[0], cmap="viridis", annot=False, linewidths=0.05)
+                axes[0].set_title(f"Attention Map at Epoch {i + 1}")
+                sns.heatmap(adj_mat, ax=axes[1], cmap="viridis", annot=False, linewidths=0.05)
+                axes[1].set_title("Adjacency Matrix of DAG")
+                plt.tight_layout()
+            else:
+                fig, axes = plt.subplots(1, 3, figsize=(18, 6))  # 1 row, 3 columns
+
+                sns.heatmap(attn[layer][0], ax=axes[0], cmap="viridis", annot=False, linewidths=0.05)
+                axes[0].set_title(f"Head 1 Attention Map at Epoch {i + 1}")
+                sns.heatmap(attn[layer][1], ax=axes[1], cmap="viridis", annot=False, linewidths=0.05)
+                axes[1].set_title(f"Head 2 Attention Map at Epoch {i + 1}")
+                sns.heatmap(adj_mat, ax=axes[2], cmap="viridis", annot=False, linewidths=0.05)
+                axes[2].set_title("Adjacency Matrix of DAG")
+                plt.tight_layout()
+
+
         # Save image
         image_path = f"{folder}/attn_l{config.num_layers}h{config.num_heads[layer]}v{config.vocab_size}ep{i}_L{layer}H{head}{task_name}.png"
         plt.savefig(image_path)
         plt.close()
         image_paths.append(image_path)
-    
+
     # Step 2: Combine images into a GIF
     frames = [Image.open(image_path) for image_path in image_paths]
-    output_gif_path = f"attnmaps_l{config.num_layers}h{config.num_heads[layer]}v{config.vocab_size}_L{layer}H{head}{task_name}.gif"
+    os.makedirs(out_folder, exist_ok=True)
+    # Get current time
+    curr_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+    output_gif_path = f"{out_folder}/l{config.num_layers}h{config.num_heads[layer]}v{config.vocab_size}_L{layer}H{head}{task_name}_{curr_time}.gif"
     
     frames[0].save(
         output_gif_path,
         save_all=True,
         append_images=frames[1:],
-        duration=500,  # Duration between frames in milliseconds
+        duration=100,  # Duration between frames in milliseconds
         loop=0  # Infinite loop
     )
     
     print(f"GIF saved at {output_gif_path}")
-
-    try:
-        shutil.rmtree(folder)
-        print(f"Folder '{folder}' and its contents removed.")
-    except FileNotFoundError:
-        print(f"Folder '{folder}' does not exist.")
-    except OSError as e:
-        print(f"Error removing folder '{folder}': {e}")
-
+    shutil.rmtree(folder, onerror=onerror)
+    print(f"Folder '{folder}' and its contents removed.")
 
 def get_pos_sim(config, model):
     range_pos_toks = torch.arange(config.seq_len).to(config.device)
@@ -122,6 +157,9 @@ def plot_bigram_icl_risk(config, train_results, task_name):
     plt.show()
 
 def plot_adj_heatmap(adj_mat):
-    sns.heatmap(adj_mat, annot=False, cmap='coolwarm', fmt='.2f', linewidths=0.5)
+    plt.figure(figsize=(6, 6))
+    sns.heatmap(adj_mat, annot=False, cmap='viridis', fmt='.2f', linewidths=0.05, cbar=False)
+    plt.yticks(rotation=0) 
+    plt.xticks(rotation=0) 
     plt.title('Matrix Heatmap')
     plt.show()

train.py
@@ -36,10 +36,10 @@ def get_train_result(**kwargs):
     return kwargs
 
 
-
 def train_causal(model, config, sampler_config, task_name):
     sampler = get_sampler(sampler_config, task_name)
     train_losses, eval_losses, eval_steps = [], [], []
+    bayes_losses = []
     attn_maps = {}
     test_data, test_prob = sampler.generate(mode="test")
 
@@ -50,12 +50,20 @@ def train_causal(model, config, sampler_config, task_name):
     def criterion(logits, probs):
         log_probs = torch.log_softmax(logits, dim=-1)
         return -torch.sum(probs * log_probs, dim=-1).mean()
+    
+    def get_bayes_loss(bayes_prob, prob):
+        return -torch.sum(prob * torch.log(bayes_prob), dim=-1).mean()
 
     optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
     
     for epoch in trange(config.num_epochs):
         model.train()
         x, prob = sampler.generate()
+        with torch.no_grad():
+            bayes_prob = sampler.bayes(x)
+            bayes_loss = get_bayes_loss(bayes_prob, prob)
+            bayes_losses.append(bayes_loss.item())
+
         optimizer.zero_grad()
         
         if config.get_attn > 0 and epoch % config.get_attn == 0:
@@ -84,7 +92,7 @@ def train_causal(model, config, sampler_config, task_name):
     return get_train_result(train_losses=train_losses, 
                             eval_losses=eval_losses, 
                             eval_steps=eval_steps, 
-                            attn_maps=attn_maps, sampler=sampler)
+                            attn_maps=attn_maps, sampler=sampler, bayes_losses=bayes_losses)
 
 
 